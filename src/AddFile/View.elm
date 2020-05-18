0d1d56806260d6ff63ce53c9a97ccfe7618dc8e9
DieMyst <dmitry.shakhtarin@fluence.ai>
2020-05-18 13:19:33 +0300

    add file

--- src/AddFile/View.elm
+++ src/AddFile/View.elm
@@ -2,21 +2,9 @@ module AddFile.View exposing (view)
 
 import AddFile.Model exposing (Model)
 import AddFile.Msg exposing (Msg(..))
-import Element
-    exposing
-        ( Element
-        , centerX
-        , column
-        , el
-        , fillPortion
-        , mouseOver
-        , padding
-        , paddingXY
-        , row
-        , spacing
-        , text
-        , width
-        )
+import Element exposing (Element, centerX, column, el, fillPortion, mouseOver, padding, paddingXY, pointer, row, spacing, text, width)
+import Element.Border exposing (dotted)
+import Element.Events exposing (onClick)
 import Element.Font as Font
 import Element.Input as Input
 import Ions.Background as BG
@@ -57,7 +45,7 @@ addFileBlock model =
     let
         addUpload =
             row [ centerX, fillWidth, BG.white ]
-                [ el [ width (fillPortion 3), padding 10, F.nearBlack ] <| Element.el [ F.size5 ] <| text "Choose file from your device"
+                [ el [  width (fillPortion 5), padding 10 ] <| el [padding 10, fillWidth, F.gray, B.width1 B.AllSides, dotted, onClick FileRequested, pointer ] <| text "Choose file from your device"
                 , Input.button
                     [ centerX
                     , width (fillPortion 2)
@@ -67,6 +55,7 @@ addFileBlock model =
                     , padding 10
                     ]
                     { onPress = Just FileRequested, label = text "Browse file" }
+                , Element.el [ width <| fillPortion 4 ] Element.none
                 ]
 
         ipfsDownload =
@@ -74,25 +63,27 @@ addFileBlock model =
                 [ B.orange
                 , B.width1 B.AllSides
                 , padding 10
-                , fillWidth
+                , width <| fillPortion 2
                 , Font.center
                 ]
                 { onPress = Just DownloadIpfs, label = text "Download" }
 
         ipfsInput =
-            Input.text [ width (fillPortion 4), centerX ]
+            Element.el [padding 10, width (fillPortion 5)] <| Input.text [ centerX ]
                 { onChange = ChangeIpfsHash
                 , text = model.ipfsHash
                 , placeholder = Just <| Input.placeholder [] <| text "Enter IPFS hash"
-                , label = Input.labelRight [ Element.centerY, width (fillPortion 1) ] <| ipfsDownload
+                , label = Input.labelHidden "Enter IPFS hash"
                 }
 
         addIpfs =
-            row [ fillWidth, centerX, BG.white, spacing 0 ] [ ipfsInput ]
+            row [ fillWidth, centerX, BG.white, spacing 0 ] [ ipfsInput, ipfsDownload, Element.el [ width <| fillPortion 4 ] Element.none ]
     in
-    column (layoutBlock ++ [ B.width1 B.AllSides, B.radius2, B.blackAlpha 100, spacing 20, F.size7 ])
+    column (layoutBlock ++ [ B.width1 B.AllSides, B.radius2, B.blackAlpha 100 ])
         [ blockTitle <| text "ADD FILE"
+        , el [padding 10, F.size5] <| text "Add file from your device or enter IPFS hash"
         , addUpload
+        , el [padding 10, F.size5] <| text "OR"
         , addIpfs
         , el [ Element.height <| Element.px 0 ] <| Element.none
         ]
