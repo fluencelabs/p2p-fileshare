b8e6dfadd0e2a55d0fba823d4feeb7753fa932e5
DieMyst <dmitry.shakhtarin@fluence.ai>
2020-05-18 13:37:52 +0300

    longer hash

--- src/Palette.elm
+++ src/Palette.elm
@@ -47,7 +47,7 @@ newTabLink url label =
 
 
 linkStyle =
-    [ accentFontColor, C.easeIn, mouseOver [ BG.washedYellow ], Font.underline ]
+    [ accentFontColor, C.easeIn, Font.underline ]
 
 
 h1 txt =
@@ -83,9 +83,9 @@ shortHash hash =
     let
         sh =
             String.concat
-                [ String.left 4 hash
+                [ String.left 6 hash
                 , "..."
-                , String.right 3 hash
+                , String.right 5 hash
                 ]
     in
     Element.el
