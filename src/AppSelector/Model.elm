5d1c70c5039c72a8d0eefc378eaa07e532ecadaf
DieMyst <dmitry.shakhtarin@fluence.ai>
2020-06-23 16:15:16 +0300

    format

--- src/AppSelector/Model.elm
+++ src/AppSelector/Model.elm
@@ -1,9 +1,11 @@
 module AppSelector.Model exposing (..)
 
+
 type App
     = FileSharing
     | NetworkMap
     | None
+
 
 appKey : App -> String
 appKey app =
@@ -17,23 +19,11 @@ appKey app =
         None ->
             "None"
 
-stringToApp : String -> App
-stringToApp key =
-    case key of
-        "File Sharing" ->
-            FileSharing
-
-        "Network Map" ->
-            NetworkMap
-
-        _ -> None
-
-
-
 
 type alias Model =
     { currentApp : App
     }
+
 
 emptyAppSelector : Model
 emptyAppSelector =
